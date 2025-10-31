1b8d0cb690c9329278ffead56b098686dd17e46a
Carlos Ruggiero <carlosruggiero6@gmail.com>
2025-10-31 17:01:42 -0500

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  apiBaseUrl: "http://localhost:8000",
+  apiBaseUrl: "https://sistema-de-optimizacion-logistica.onrender.com",
   mapboxToken: "pk.eyJ1IjoicnVnZ2VyaW5pIiwiYSI6ImNtZnZtc3JlcTAwbmwyaXBvOHB0ZW9vZ28ifQ.aTieIIO-E7Q4Hfryf5yLjQ",
 };
